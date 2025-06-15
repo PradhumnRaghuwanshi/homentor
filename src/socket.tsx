5e0c87dfe874f207313263754530a163f63b455c
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-06-15 15:55:49 +0530

    payment

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,6 +1,6 @@
 // socket.js or socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Make sure this matches your backend URL
+const socket = io("https://homentor-backend.onrender.com"); // Make sure this matches your backend URL
 
 export default socket;
