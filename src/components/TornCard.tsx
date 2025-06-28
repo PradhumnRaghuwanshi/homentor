357420992d5e054fe2ef73b3a102ebf5ac33bac9
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-06-28 19:35:44 +0530

    update

--- src/components/TornCard.tsx
+++ src/components/TornCard.tsx
@@ -68,9 +68,8 @@ const TornCard = ({ mentor }) => {
   const sendCallRequest = ()=>{
     axios.post("https://homentor-backend.onrender.com/api/mentor-call", {
       name: mentor.fullName,
-      phone: mentor.phone,
-      date : Date.now
-    })
+      phone: mentor.phone
+    }).then((res)=> console.log(res)).catch((err)=> console.log(err))
   }
   return (
     <div className="relative animate-shake origin-top w-[100%] flex overflow-hidden flex-col items-center bg-[papayawhip] rounded-lg  shadow-[0_0_20px_-5px_black]">
@@ -157,11 +156,13 @@ const TornCard = ({ mentor }) => {
           </div>
         </button>
         <button
-          onClick={() => makeCall()}
+          // onClick={() => makeCall()}
           className="border bg-blue-opacity px-1 py-0.5 border-mentor-blue-500 rounded-[2px] bg-mentor-blue-500 text-white mentor-icons1-sm from-homentor-chat to-homentor-chatHover hover:from-homentor-chatHover hover:to-homentor-chat transition-all duration-300 flex items-center justify-center overflow-hidden "
           title="Chat with mentor"
         >
           <a
+           onClick={()=> sendCallRequest()}
+            href={`tel:${mentor.phone}`}
             // href={`tel:${mentor.phone}`}
             className="inline lg:text-md text-[11px]"
           >
